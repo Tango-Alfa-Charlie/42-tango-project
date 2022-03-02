efa163614fd39177a744736c65a339c69370086b
Tango-Alfa-Charlie <100702334+Tango-Alfa-Charlie@users.noreply.github.com>
2022-03-02 16:12:36 +1030

    Create greetings_world.c

---
+++ Code3/greetings_world.c
@@ -0,0 +1,15 @@
+// Simple C program to display "Greetings World"
+  
+// Header file for input output functions
+#include <stdio.h>
+  
+// main function -
+// where the execution of program begins
+int main()
+{
+  
+    // prints greetings world
+    printf("Greetings World");
+  
+    return 0;
+}
