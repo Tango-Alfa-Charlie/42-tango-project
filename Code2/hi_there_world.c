0a42fec13937df74a5910ed8ab387d7bc10390ea
Tango-Alfa-Charlie <100702334+Tango-Alfa-Charlie@users.noreply.github.com>
2022-03-02 16:17:42 +1030

    Create hi_there_world.c

---
+++ Code2/hi_there_world.c
@@ -0,0 +1,15 @@
+// Simple C program to display "Hi There World"
+  
+// Header file for input output functions
+#include <stdio.h>
+  
+// main function -
+// where the execution of program begins
+int main()
+{
+  
+    // prints hi there world
+    printf("Hi There World");
+  
+    return 0;
+}
